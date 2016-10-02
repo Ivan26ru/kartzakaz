817e9c90eb724d5cc431e5e99eb321ae1f57c103
dev7 <dev7@idea-labs.ru>
2016-10-03 02:03:17 +0300

    форма поиска

--- header.php
+++ header.php
@@ -83,6 +83,13 @@
 					  			);
 								wp_nav_menu($args); // выводим верхнее меню
 							?>
+													                            <!-- кнопки справа-->
+                            <!-- добавил другую форму для поиска, чтоб с главной всегда искало и метод get -->
+                            <form class="navbar-form navbar-right" method="get" role="search" action="<?php echo home_url( '/' ) ?>" >
+                            <!-- сама форма поиска -->
+                            <?php get_search_form() ?>
+                            </form>
+                            <!-- /кнопки справа -->
 						</div>
 					</nav>
 				<!-- </div> -->
