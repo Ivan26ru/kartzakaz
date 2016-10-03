272a25d7cf122d86d3fe1ea001a0cbf75e987e0b
Ivan26ru <it-1ivan@yandex.ru>
2016-10-03 14:35:36 +0300

    миниатюра поста и кнопки следующий и предыдущий

--- single.php
+++ single.php
@@ -12,15 +12,20 @@ get_header(); // подключаем header.php ?>
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); // старт цикла ?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> <?php // контэйнер с классами и id ?>
 						<h1 class="text-center"><?php the_title(); // заголовок поста ?></h1>
+						<p class="text-center">
+						<?php the_post_thumbnail(); ?>
+						</p>
 						<div class="meta">
 							<p>Категории: <?php the_category(',') ?></p> <?php // ссылки на категории в которых опубликован пост, через зпт ?>
 						</div>
 						<?php the_content(); // контент ?>
 					</article>
 				<?php endwhile; // конец цикла ?>
-				<?php previous_post_link('%link', '<- Предыдущий пост: %title', TRUE); // ссылка на предыдущий пост ?>
-				<?php next_post_link('%link', 'Следующий пост: %title ->', TRUE); // ссылка на следующий пост ?>
-				<?php //if (comments_open() || get_comments_number()) comments_template('', true); // если комментирование открыто - мы покажем список комментариев и форму, если закрыто, но кол-во комментов > 0 - покажем только список комментариев ?>
+				<hr>
+				<ul class="pager">
+				  <li class="previous"><?php previous_post_link('%link', '<- Предыдущий пост: %title', TRUE); // ссылка на предыдущий пост ?> </li>
+				  <li class="next"><?php next_post_link('%link', 'Следующий пост: %title ->', TRUE); // ссылка на следующий пост ?> </li>
+				</ul>
 			</div>
 			<?php get_sidebar(); // подключаем sidebar.php ?>
 		</div>
