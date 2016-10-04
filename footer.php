90d277b88c094c9decac32d4e278b7ebaa751db1
dev7 <dev7@idea-labs.ru>
2016-10-04 03:51:57 +0300

    добавил hr и небольшую контактную инфу

--- footer.php
+++ footer.php
@@ -5,10 +5,19 @@
  * @subpackage your-clean-template-3
  */
 ?>
+<hr>
 	<footer>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
+				<div class="text-right">
+										<a href="tel:+79187959105" class="tel"><span class="glyphicon glyphicon-earphone"></span>7(918)795-91-05</a><br>
+					<a href="mailto:info@kartzakaz.ru">
+						<span class="glyphicon glyphicon-envelope"></span>
+						info@kartzakaz.ru</a>
+					<p><b>покупка картриджей</b><br>
+					оптом и в розницу</p>
+				</div>
 					<?php $args = array( // опции для вывода нижнего меню, чтобы они работали, меню должно быть создано в админке
 						'theme_location' => 'bottom', // идентификатор меню, определен в register_nav_menus() в function.php
 						'container'=> false, // обертка списка, false - это ничего
