b295bee8ad61d116163b151e1a5b9efd001d5a2f
ivan26ru <it-1ivan@yandex.ru>
2016-10-11 00:44:41 +0300

    добавлены некоторые фирмы

--- list-item.php
+++ list-item.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Карусель (list-item.php)
+ * лого фирм (list-item.php)
  * @package WordPress
  * @subpackage your-clean-template-3
  */
 ?>
 <!-- значки фирм принтеров -->
 <div class="b-category b-category_gal" tag="cat-list">
+
         <div class="category__item">
-            <a href="<?php echo home_url( '/' ) ?>category/zakupka/brother/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/brother.png" alt="Brother"></a>
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/hp/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/hp.png" alt="HP"></a>
         </div>
 
         <div class="category__item">
@@ -16,15 +17,7 @@
         </div>
 
         <div class="category__item">
-            <a href="<?php echo home_url( '/' ) ?>category/zakupka/hp/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/hp.png" alt="HP"></a>
-        </div>
-
-        <div class="category__item">
-            <a href="<?php echo home_url( '/' ) ?>category/zakupka/kyocera/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/kyocera.png" alt="Kyocera-Mita"></a>
-        </div>
-
-        <div class="category__item">
-            <a href="<?php echo home_url( '/' ) ?>category/zakupka/panasonic/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/panasonic.png" alt="Panasonic"></a>
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/xerox/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/xerox.png" alt="Xerox"></a>
         </div>
 
         <div class="category__item">
@@ -32,6 +25,43 @@
         </div>
 
         <div class="category__item">
-            <a href="<?php echo home_url( '/' ) ?>category/zakupka/xerox/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/xerox.png" alt="Xerox"></a>
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/brother/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/brother.png" alt="Brother"></a>
         </div>
+
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/panasonic-kartridzhi-i-termoplenki/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/panasonic.png" alt="Panasonic"></a>
+        </div>
+
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/ricoh/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/ricoh.png" alt="Ricoh"></a>
+        </div>
+
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/lexmark/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/lexmark.png" alt="Lexmark"></a>
+        </div>
+
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/oki/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/oki.png" alt="OKI"></a>
+        </div>
+
+
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/kyocera-mita/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/kyocera.png" alt="Kyocera-Mita"></a>
+        </div>
+
+<!--         <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/epson/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/epson.png" alt="Epson"></a>
+        </div> -->
+<!--
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/sharp/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/sharp.png" alt="Sharp"></a>
+        </div>
+
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/toshiba/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/toshiba.png" alt="Toshiba"></a>
+        </div>
+
+        <div class="category__item">
+            <a href="<?php echo home_url( '/' ) ?>category/zakupka/konika-minolta/"><img src="<?php echo get_template_directory_uri(); ?>/img/list-item/konika.png" alt="KONIKA MINOLTA"></a>
+        </div> -->
 </div>
