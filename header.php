b9e0f65f3f4140fd9cccb9d5b1c7176db89c6857
ivan26ru <it-1ivan@yandex.ru>
2016-10-13 17:48:51 +0300

    коммент, что я создал сайт)

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); // вывод атрибутов языка ?>>
 <head>
+<!-- РАЗРАБОТКА САЙТОВ https://vk.com/ivan26ru -->
 	<meta charset="<?php bloginfo( 'charset' ); // кодировка ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
